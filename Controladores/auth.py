af442378317135e437e7060cf8fe4d0eda2f153b
Jackelin <jackelinvasquezguzman@gmail.com>
2026-03-21 16:28:56 -0600

    [6.1] Creación Login Corección

--- Controladores/auth.py
+++ Controladores/auth.py
@@ -6,7 +6,6 @@ from pathlib import Path
 class Auth:
     def __init__(self):
         self.admin_actual = None
-        # BUSCAR LA RAÍZ DEL PROYECTO (Subir un nivel desde Controladores)
         self.raiz = Path(__file__).resolve().parent.parent
         self.carpeta_datos = self.raiz / "Datos_json"
 
@@ -25,7 +24,6 @@ class Auth:
         try:
             with open(archivo, 'r', encoding='utf-8') as f:
                 datos = json.load(f)
-                # Asegúrate de que la ruta a Modelos.Clase_admin sea correcta
                 from Modelos.Clase_admin import Admin
                 self.admin = Admin(datos['usuario'], datos['contraseña'])
                 self.admin.nombre = datos.get('nombre', 'Administrador')
@@ -37,7 +35,7 @@ class Auth:
         from Modelos.Clase_admin import Admin
         self.admin = Admin("admin", "admin123")
         self._guardar_admin()
-        print(f"✅ Archivo JSON creado en: {self.carpeta_datos / 'admin.json'}")
+        print(f"Archivo JSON creado en: {self.carpeta_datos / 'admin.json'}")
 
     def _guardar_admin(self):
         archivo = self.carpeta_datos / "admin.json"
