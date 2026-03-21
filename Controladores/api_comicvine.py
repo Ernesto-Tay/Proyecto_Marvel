c7635de9886ccb519b3d982d80ca8dd06f9c074b
Ernesto Tay <jetayr@correo.url.edu.gt>
2026-03-20 22:33:51 -0600

    Edición de API e iniciación de buscadores (para las estructuras)

--- Controladores/api_comicvine.py
+++ Controladores/api_comicvine.py
@@ -1,9 +1,11 @@
 # Aqui vaos a controlar la API de Comic Vine, quiere decir
 #aqui vamos a hacer las consultas, obtener datos, procesar los comics y asi,
 #para esto cada uhno debe solicitar su API en comic vine :D
+import urllib.request
+import json
+import time
 
 class ComicVineAPI:
-
     def __init__(self, api_key):
         self.api_key = api_key
         self.base_url = "https://comicvine.gamespot.com/api"
@@ -11,4 +13,21 @@ class ComicVineAPI:
         self.min_request_interval = 0.2  # 5 peticiones por segundo para que nos nos vanee comic vine xd
 
         # Headers para que Comic Vine nos acepte
-        self.headers = {"User-Agent': 'Proyecto_Marvel/1.0 (Proyecto Universitario)"}
+        self.headers = {"User-Agent": "Proyecto_Marvel/1.0 (Proyecto Universitario)"}
+
+    def control_tiempo(self):
+        lapso = time.time() -  self.last_request_time
+        if lapso < self.min_request_interval:
+            time.sleep(self.min_request_interval-lapso)
+
+    def extraccion(self):
+        req = urllib.request.Request(f"{self.base_url}/issues/?api_key={self.api_key}&format=json&filter=publisher:Marvel", headers=self.headers)
+        with urllib.request.urlopen(req) as resp:
+            data = json.loads(resp.read().decode())
+        return data
+
+
+
+
+
+
---
+++ Controladores/gestor_datos.py
@@ -0,0 +1,6 @@
+class Buscar:
+    def __init__(self, datab):
+        self.datab = datab
+
+    def buscar_comic(self, nombre, fecha = None):pass
+    def buscar_personaje(self, nombre):pass
